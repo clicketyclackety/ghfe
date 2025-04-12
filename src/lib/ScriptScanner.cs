22549b26aaaf2c793cf3b18d25d17fa5dc52b42d
Krzysztof Nazar <krzysztof.a.nazar@gmail.com>
2025-04-12 16:07:49 +0200

    Set up the scriptscanner method

---
+++ src/lib/ScriptScanner.cs
@@ -0,0 +1,32 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+using Grasshopper.Kernel;
+using Grasshopper.Kernel.Special;
+
+namespace lib
+{
+    public static class ScriptScanner
+    {
+        public const string UIGroupName = "UI";
+
+        public static bool TryGetUIGroup(GH_Document document, out GH_Group group)
+        {
+            var activeObjects = new List<GH_ActiveObject>();
+            var allObjects = document.Objects;
+            foreach (var obj in allObjects) {
+                if (obj is GH_Group) {
+                    if (obj.Name == UIGroupName) {
+                        return true;
+                    }
+                    var groupObj = obj as GH_Group;
+                    groupObj.
+                    activeObjects.Add(scriptInstance);
+                }
+            }
+            return activeObjects;
+        }
+    }
