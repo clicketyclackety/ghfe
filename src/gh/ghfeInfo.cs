ad012e013ea173af24c923f4dc22c1da9845c931
Callum Sykes <callumsykes@protonmail.com>
2025-04-12 13:06:30 +0100

    start

---
+++ src/gh/ghfeInfo.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Drawing;
+using Grasshopper;
+using Grasshopper.Kernel;
+
+namespace gh;
+
+public class ghfeInfo : GH_AssemblyInfo
+{
+  public override string Name => "ghfe Info";
+
+  //Return a 24x24 pixel bitmap to represent this GHA library.
+  public override Bitmap Icon => null;
+
+  //Return a short string describing the purpose of this GHA library.
+  public override string Description => "";
+
+  public override Guid Id => new Guid("a185afcd-6cde-43a5-9662-3a912b59397e");
+
+  //Return a string identifying you or your company.
+  public override string AuthorName => "";
+
+  //Return a string representing your preferred contact details.
+  public override string AuthorContact => "";
+
+  //Return a string representing the version.  This returns the same version as the assembly.
+  public override string AssemblyVersion => GetType().Assembly.GetName().Version.ToString();
+}
---
+++ src/rhino/ghferCommand.cs
@@ -0,0 +1,26 @@
+﻿using Rhino;
+using Rhino.Commands;
+
+namespace rn;
+
+public class ghferCommand : Command
+{
+    public ghferCommand()
+    {
+        // Rhino only creates one instance of each command class defined in a
+        // plug-in, so it is safe to store a refence in a static property.
+        Instance = this;
+    }
+
+    ///<summary>The only instance of this command.</summary>
+    public static ghferCommand Instance { get; private set; }
+
+    ///<returns>The command name as it appears on the Rhino command line.</returns>
+    public override string EnglishName => "ghferCommand";
+
+    protected override Result RunCommand(RhinoDoc doc, RunMode mode)
+    {
+        RhinoApp.WriteLine("The {0} command is under construction.", EnglishName);
+        return Result.Success;
+    }
+}
