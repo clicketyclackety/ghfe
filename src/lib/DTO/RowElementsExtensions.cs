134309f141874c026a2fc51a265f69c61c496e1d
Krzysztof Nazar <krzysztof.a.nazar@gmail.com>
2025-04-12 21:47:16 +0200

    tidy up

--- src/lib/DTO/RowElementsExtensions.cs
+++ src/lib/DTO/RowElementsExtensions.cs
@@ -11,17 +11,15 @@ namespace lib.DTO
     {
         public static string DisplayTree(this IRowElement elem, string indent = "")
         {
-            // Start with the current element name.
             string result = $"{indent}{elem.Name}:";
 
-            // If this is a RowGroup, recursively build strings for its children.
             if (elem is RowGroup group) {
                 foreach (var child in group.Children) {
                     result += "\n";
                     result += child.DisplayTree(indent + "  ");
                 }
             }
-            // If this is a RowLeaf, optionally display its components.
+
             else if (elem is RowLeaf leaf) {
                 result += "  ";
                 result += string.Join(",", leaf.Components.Select(c => c.NickName));
