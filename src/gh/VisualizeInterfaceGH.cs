97a1610c40e03d6c1f7a0b4b792089cab9294734
Krzysztof Nazar <krzysztof.a.nazar@gmail.com>
2025-04-13 10:54:35 +0200

    Component to visualize UI

--- src/gh/VisualizeInterfaceGH.cs
+++ src/gh/VisualizeInterfaceGH.cs
@@ -25,11 +25,17 @@ public class VisualizeInterfaceGH : GH_Component
     protected override void RegisterInputParams(GH_Component.GH_InputParamManager pManager)
     {
         pManager.AddTextParameter("Group name", "group name", "group name", GH_ParamAccess.item, "UI");
+        pManager.AddPointParameter("Origin", "origin", "origin", GH_ParamAccess.item, new Point3d(0, 0, 0));
+        pManager.AddNumberParameter("Width", "width", "width", GH_ParamAccess.item, 100);
+        pManager.AddNumberParameter("Row Height", "Row height", "height", GH_ParamAccess.item, 100);
+        pManager.AddNumberParameter("Indent", "indent", "indent", GH_ParamAccess.item, 10);
     }
 
     protected override void RegisterOutputParams(GH_Component.GH_OutputParamManager pManager)
     {
-        //pManager.AddTextParameter("Hierarchy", "Hierarchy", "Hierarchy of the group.", GH_ParamAccess.item);
+        pManager.AddCurveParameter("Outlines", "Outlines", "Outlines", GH_ParamAccess.list);
+        pManager.AddTextParameter("Labels", "Labels", "Labels", GH_ParamAccess.list);
+        pManager.AddCurveParameter("Margins", "Margins", "Margins", GH_ParamAccess.list);
     }
 
     /// <summary>
@@ -43,17 +49,31 @@ public class VisualizeInterfaceGH : GH_Component
         DA.GetData(0, ref groupName);
 
         var ghDoc = OnPingDocument();
+        var origin = new Point3d(0, 0, 0);
+        DA.GetData(1, ref origin);
+        double totalWidth = 100;
+        DA.GetData(2, ref totalWidth);
+        double leafHeight = 100;
+        DA.GetData(3, ref leafHeight);
+        double indent = 10;
+        DA.GetData(4, ref indent);
+        var outlines = new List<Polyline>();
+        var labels = new List<string>();
+        var margins = new List<Line>();
 
         if (ScriptScanner.TryGetGroup(ghDoc, groupName, out var group)) {
             var hierarchy = ScriptScanner.BuildGroupHierarchy(ghDoc, group);
             var rowGroup = hierarchy.ToRowGroup();
-            var description = rowGroup.DisplayTree();
-            //DA.SetData(0, description);
+            _ = rowGroup.DrawHierarchy(origin, totalWidth, leafHeight, indent, outlines, labels, margins);
         }
 
         else {
             AddRuntimeMessage(GH_RuntimeMessageLevel.Error, "Group not found");
         }
+
+        DA.SetDataList(0, outlines);
+        DA.SetDataList(1, labels);
+        DA.SetDataList(2, margins);
 
     }
     protected override System.Drawing.Bitmap Icon => null;
--- src/lib/DTO/RowElementsExtensions.cs
+++ src/lib/DTO/RowElementsExtensions.cs
@@ -31,6 +31,47 @@ namespace lib.DTO
             return result;
         }
 
+        public static double DrawHierarchy(this RowGroup uiRowGroup, Point3d origin, double totalWidth, double leafHeight, double indent, List<Polyline> outlines, List<string> labels, List<Line> margins)
+        {
+            double currentY = origin.Y;
 
+            // Iterate through each row element in the list
+            foreach (var element in uiRowGroup.Children) {
+                if (element is RowLeaf leaf) {
+                    int cellCount = (leaf.Components != null && leaf.Components.Count > 0) ? leaf.Components.Count : 1;
+                    double cellWidth = totalWidth / cellCount;
+
+                    for (int i = 0; i < cellCount; i++) {
+                        // Define the bottom-left and top-right points for the cell rectangle.
+                        Point3d cellOrigin = new Point3d(origin.X + i * cellWidth, currentY, 0);
+                        Point3d cellLowerRight = new Point3d(origin.X + (i + 1) * cellWidth, currentY - leafHeight, 0);
+
+                        Rectangle3d cellRect = new Rectangle3d(Plane.WorldXY, cellOrigin, cellLowerRight);
+                        string leafName = leaf.Name;
+                        outlines.Add(cellRect.ToPolyline());
+                        labels.Add(leafName);
+                    }
+
+                    currentY -= leafHeight;
+                }
+                else if (element is RowGroup group) {
+                    double groupStartY = currentY;
+
+                    // Optionally, you might want to draw the group’s name (or a container rectangle) in the margin or above its contents.
+                    // In this example, we continue drawing the children indented by the specified value.
+                    // The available width for children is reduced by the indent.
+                    currentY = DrawHierarchy(group as RowGroup, new Point3d(origin.X + indent, currentY, 0), totalWidth - indent, leafHeight, indent, outlines, labels, margins);
+
+                    // After the recursive call, currentY marks the end of the last nested row.
+                    // Next, we draw a vertical line that visually indicates the indentation “guide”.
+                    // We place the line at a fixed offset from the original left edge.
+                    Point3d lineStart = new Point3d(origin.X + indent * 0.5, groupStartY, 0);
+                    Point3d lineEnd = new Point3d(origin.X + indent * 0.5, currentY, 0);
+                    margins.Add(new Line(lineStart, lineEnd));
+                }
+            }
+            // Return the Y position after all elements have been rendered.
+            return currentY;
+        }
     }
 }
